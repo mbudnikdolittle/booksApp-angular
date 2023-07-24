a7b60bb5aae49466d0167dadd1a306ee5dccc04b
mbudnikdolittle <mbudnik@deloittece.com>
2023-07-24 15:14:10 +0200

    add http

--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
   styleUrls: ['./books-list.component.css']
 })
-export class BooksListComponent {
+export class BooksListComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getBooks()
+  }
+
+  getBooks() {
+    this.http.get('/api/books').subscribe(data => console.log(data))
+  }
 
 }
